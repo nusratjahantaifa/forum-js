237ca15535cfc71dc73608bb61c9633cf62b5609
nusratjahantaifa <nusratjahantaifa05@gmail.com>
2025-10-18 19:34:29 +0600

    added styls

--- js/forum.js
+++ js/forum.js
@@ -1,4 +1,35 @@
-function myFunction() {
-  alert("Button was clicked!");
+//  function myFunction() {
+//   alert("Button was clicked!");
+// }
+const loadPost = async () =>{
+    const res = await fetch(' https://openapi.programming-hero.com/api/retro-forum/posts');
+const data = await res.json();
+console.log(data);
 }
-console.log();
+loadPost();
+
+
+  const viewCount = document.querySelector('.fa-eye').nextSibling;
+  let views = 1568;
+  setInterval(() => {
+    views += Math.floor(Math.random() * 3);
+    viewCount.textContent = ` ${views.toLocaleString()}`;
+  }, 3000);
+const loadCard = async () =>{
+  const res = await fetch('https://openapi.programming-hero.com/api/retro-forum/latest-posts');
+  const data = await res.json();
+  console.log(data);
+ 
+  //  const container = document.getElementById('card-container');
+
+}
+//handleSearch btn
+const handleSearch = () =>{
+  // console.log('search handle')
+  const searchField = document.getElementById('search');
+  const searchText = searchField.value;
+  console.log(searchText);
+loadCard(search);
+}
+ loadCard();
+
